daac7c48d7996f4c3cac6b7c9012bb19c5e43096
Smeezorline <56321563+grevz@users.noreply.github.com>
2026-01-22 19:04:50 +0300

    Create iiko-core.user.js

---
+++ iiko-core.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         iiko Core Script
+// @namespace    https://github.com/grevz/modify_iikofranchise
+// @version      1.0.0
+// @description  Базовый скрипт для iiko
+// @match        https://franchise-1.iiko.it/*
+// @updateURL    https://raw.githubusercontent.com/grevz/modify_iikofranchise/main/iiko-core.user.js
+// @downloadURL  https://raw.githubusercontent.com/grevz/modify_iikofranchise/main/iiko-core.user.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    if (location.hostname === 'franchise-1.iiko.it') {
+        console.log('Ты во франшизе. Удачи!');
+    }
+})();
